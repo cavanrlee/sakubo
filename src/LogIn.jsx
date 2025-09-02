b3387788bd4bc374c966a67400228b4f51b2eb91
Andongispapsy <your@email.com>
2025-09-02 21:55:53 +0800

    saKubo-HomePage-Wireframe

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App1.css'
+import './LogIn.css'
 
-function App1() {
+function LogIn() {
     const [count, setCount] = useState(0)
 
     return (
@@ -94,4 +94,4 @@ function App1() {
     )
 }
 
-export default App1
+export default LogIn
