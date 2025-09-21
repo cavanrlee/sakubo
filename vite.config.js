b2b0d07df9fad07e6f06f44d8806ea80bd1845eb
Rolly Cavan <cavanrlee2@gmail.com>
2025-09-22 07:32:50 +0800

    updates for structures

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-});
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    server: {
+        proxy: {
+            '/users': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                secure: false,
+                // rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
+})
