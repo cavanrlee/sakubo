29b61307bf5e1e53bdc34aa330ad8698fdf448ab
Rolly Cavan <cavanrlee2@gmail.com>
2026-03-20 22:51:45 +0800

    adjustments in register page navigation 3

--- src/pages/Login/LoginController.js
+++ src/pages/Login/LoginController.js
@@ -2,7 +2,7 @@ import axios from "@/api/axios.js";
 
 export const loginUser = async(formData) => {
 	await axios.get("/sanctum/csrf-cookie");
-	const response = await axios.post("api/login",formData);
+	const response = await axios.post("/login", formData);
 
 	return response.data.data;
 }
