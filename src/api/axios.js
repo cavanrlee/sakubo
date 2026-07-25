d04848275d944f48a5e3b01387e00471ccd46f73
Rolly Cavan <cavanrlee2@gmail.com>
2026-07-26 02:02:07 +0800

    fixed cross site 2

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,12 +5,12 @@ import { Capacitor } from "@capacitor/core";
 const getBaseURL = () => {
     // 1. Mobile app sa Capacitor (Android/iOS)
     if (Capacitor.isNativePlatform()) {
-        return "/";
+        return "https://sakubo-web.onrender.com";
     }
 
     // Production web
     if (import.meta.env.PROD) {
-        return "/";
+        return "https://sakubo-web.onrender.com";
     }
 
     // Local Development
