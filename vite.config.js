9cbe01254243f6fb467c6ab53a963cd4805b8181
Rolly Cavan <cavanrlee2@gmail.com>
2026-07-26 01:39:53 +0800

    fixed cross site

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path' // <--- add this
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Ituro ito papunta sa public folder ng iyong Laravel project
+    outDir: '../sakubo-web/public',
+    emptyOutDir: false,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // <--- now @ maps to src folder
